4e49b2619012d0784906d86c2ce3c3462c8f4c54
Ahmet ERYILMAZ <uzali.kafasi@gmail.com>
2022-05-22 18:45:46 +0300

    includes

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -6,9 +6,12 @@
 /*   By: aeryilma <aeryilma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 22:36:49 by aeryilma          #+#    #+#             */
-/*   Updated: 2022/05/12 06:30:13 by aeryilma         ###   ########.fr       */
+/*   Updated: 2022/05/21 14:37:26 by aeryilma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
 #include "ft_printf.h"
 
@@ -19,9 +22,22 @@ typedef struct s_stack
 }	t_stack;
 
 //OPERATIONS
+//SWAP
 void	sa(t_stack **a);
 void	sb(t_stack **b);
-void	ss(t_stack **a);
-
+void	ss(t_stack **a, t_stack **b);
+//ROTATE
+void	ra(t_stack **a);
+void	rb(t_stack **b);
+void	rr(t_stack **a, t_stack **b);
+//REVERSEROTATE
+void	rra(t_stack **a);
+void	rrb(t_stack **b);
+void	rrr(t_stack **a, t_stack **b);
+//PUSH
+void	pa(t_stack **a, t_stack **b);
+void	pb(t_stack **a, t_stack **b);
 
 int	inputkontrol(char *input, int *arg);
+
+#endif
