8e97c73c875643bea377f946b2f50d06d8e6b981
Ahmet ERYILMAZ <61971131+kuvarti@users.noreply.github.com>
2022-06-13 12:31:36 +0300

    smaller code for atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,18 +6,18 @@
 /*   By: aeryilma <aeryilma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 16:26:38 by aeryilma          #+#    #+#             */
-/*   Updated: 2022/02/17 11:11:06 by aeryilma         ###   ########.fr       */
+/*   Updated: 2022/06/13 12:28:56 by aeryilma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "limits.h"
 
-static int	ft_isspace(char chr)
+static int	ft_isspace(const char *chr, int *index)
 {
-	if ((chr >= 9 && chr <= 13) || chr == ' ')
-		return (1);
-	return (0);
+	*index = 0;
+	while ((chr[index] >= 9 && chr[index] <= 13) || chr[index] == ' ')
+		index++;
 }
 
 static int	ft_isaret(char c, int *index)
@@ -40,25 +40,22 @@ int	ft_atoi(const char *nptr)
 	int	index;
 	int	isaret;
 
-	index = 0;
 	ret_val = 0;
 	if (*nptr == '\0')
 		return (0);
-	while (ft_isspace(nptr[index]))
-		index++;
+	ft_isspace(nptr, &index);
 	isaret = ft_isaret(nptr[index], &index);
 	while (nptr[index] == '0')
 		index++;
 	while (nptr[index] >= '0' && nptr[index] <= '9')
 	{
-		ret_val = (ret_val * 10) + (nptr[index] - '0');
+		ret_val = (ret_val * 10) + (nptr[index++] - '0');
 		if (ret_val <= 0)
 		{
 			if (isaret == -1)
 				return (0);
 			return (-1);
 		}
-		index++;
 	}
 	return (ret_val * isaret);
 }
