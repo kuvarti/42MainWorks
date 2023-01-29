8912a832d6e62496fa020316a7a2e4507d040ec2
Kuvarti <uzali.kafasi@gmail.com>
2023-01-30 02:55:56 +0300

    everthink ok with read file

--- cub3d/src/utils/filemanagement/mapgenerate.c
+++ cub3d/src/utils/filemanagement/mapgenerate.c
@@ -6,7 +6,7 @@
 /*   By: aeryilma <aeryilma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 19:49:04 by aeryilma          #+#    #+#             */
-/*   Updated: 2023/01/29 04:59:04 by aeryilma         ###   ########.fr       */
+/*   Updated: 2023/01/30 02:52:29 by aeryilma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,34 +42,68 @@ int	fill_mapspaces(char **map, int xlen)
 	return (1);
 }
 
-char	**getmap(char *file, size_t ylen)
+char	*getmaptext(int fd)
 {
-	int		fd;
-	int		i;
-	char	**map;
+	char	*str;
+	char	*check;
 
-	fd = open(file, O_RDONLY);
-	if (fd == -1)
+	check = 0;
+	while (1)
+	{
+		if (check)
+			free(check);
+		check = get_next_line(fd);
+		if (ft_strncmp(check, "\n", 1))
+			break ;
+	}
+	str = 0;
+	while (1)
+	{
+		str = ft_gnl_strjoin(str, check);
+		free(check);
+		check = get_next_line(fd);
+		if (!check)
+			break ;
+	}
+	return (str);
+}
+
+char	**getmap(t_cub3d *game, int fd)
+{
+	size_t	tmp;
+	char	**map;
+	char	*text;
+	char	*check;
+
+	text = getmaptext(fd);
+	check = ft_strnstr(text, "\n\n", ft_strlen(text));
+	if (check)
 		return (0);
-	map = (char **)malloc(sizeof(char *) * ylen);
-	i = 0;
-	while (++i < 9)
-		free(get_next_line(fd));
-	i = 0;
-	while (--ylen > 0)
-		map[i++] = get_next_line(fd);
-	map[i] = 0;
+	map = ft_split(text, '\n');
+	free(text);
+	game->map->y = 0;
+	game->map->x = 0;
+	while (map[game->map->y])
+	{
+		tmp = ft_strlen(map[game->map->y]);
+		if (game->map->x < tmp)
+			game->map->x = tmp;
+		game->map->y++;
+	}
 	return (map);
 }
 
 int	loadsprites(int fd, t_cub3d *game)
 {
 	char	*line;
+	int		ret;
 
+	ret = 0;
 	line = 0;
 	while (1)
 	{
 		line = get_next_line(fd);
+		ret++;
 		if (importxpm(game, line))
 			return (0);
 		free(line);
@@ -77,7 +111,7 @@ int	loadsprites(int fd, t_cub3d *game)
 				&& game->texture.xpm[3] && game->texture.bot && game->texture.top)
 			break ;
 	}
-	return (1);
+	return (ret);
 }
 
 int	mapgenerate(t_cub3d *game, char *file)
@@ -87,17 +121,15 @@ int	mapgenerate(t_cub3d *game, char *file)
 	game->map = malloc(sizeof(t_map));
 	if (ft_strlen(file) < 5 || ft_strrcmp(file, ".cub") != 0)
 		return (!printf("Wrong file format: correct file format: file.cub\n"));
-
 	fd = open(file, O_RDONLY);
 	if (fd < 0)
 		return (!printf("File Not Found :%s\n", file));
 	if (!loadsprites(fd, game))
-		return(0);
-
-	game->map->map = getmap(file);
-	if (!game->map)
+		return(!printf("Some Error Accuired!\n"));
+	game->map->map = getmap(game, fd);
+	if (!game->map->map)
 		return (!printf("Some Error Accuired.\n"));
-	if (!fill_mapspaces(game->map->map, game->map->y))
+	if (!fill_mapspaces(game->map->map, game->map->x))
 		return (!printf("Unsupported character detected.\n"));
 	if (!mapcheck(game->map->map))
 		return (!printf("Map & Player must be covered by wall(s)\n"));
